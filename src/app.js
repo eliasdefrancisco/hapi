0fea345444cc6c9ae22fab745d280af4323af20e
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-17 17:02:18 +0100

    serving txt file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const hapi = require('@hapi/hapi')
+const path = require('path')
+// const inert = require('inert')
+
 
 const init = async () => {
     const server = new hapi.Server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: path.join(__dirname, 'public')
+            }
+        }
     })
+
+    // await server.register(require('@hapi/vision'))
+    // await server.register(inert)
+
+    // server.views({
+        
+    // })
+
     await server.start()
     console.log('Server running on: ', server.info.uri)
 
@@ -13,6 +29,33 @@ const init = async () => {
         path: '/',
         handler: (request, h) => {
             return '<h1>Hello world</h1>'
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About'
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/hello/{user}',
+        handler: (request, h) => {
+            return `
+                <h1>Hello ${request.params.user}!</h1>
+                <p>que ase pare</p>
+            `
+        }
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/text.txt',
+        handler: (request, h) => {
+            return h.file('./text.txt')
         }
     })
 }
